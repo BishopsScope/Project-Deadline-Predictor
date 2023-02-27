Fixing computation

methods.py
@@ -6,25 +6,25 @@ import os
 import decimal
 
 
-def file_exists(filename):
-    """ Check if the file exists """
+# def file_exists(filename):
+#     """ Check if the file exists """
 
-    if os.path.exists(filename):
-        # Load previous data
-        user_input_data = np.loadtxt(filename, delimiter=',')
+#     if os.path.exists(filename):
+#         # Load previous data
+#         user_input_data = np.loadtxt(filename, delimiter=',')
 
-        # There's no need for a time_amt since we've already collected data
-        time_amt = None
+#         # There's no need for a time_amt since we've already collected data
+#         # time_amt = None
 
-    else:
-        # Create a new array
-        user_input_data = np.array([])
+#     else:
+#         # Create a new array
+#         user_input_data = np.array([])
 
-        # We haven't collected data yet, so we need the user to guess the first amount of time
-        time_amt = float(input(
-            "Since the dataset is new, how many time units (in minutes) do you expect to complete this task? "))
+#         # We haven't collected data yet, so we need the user to guess the first amount of time
+#         # time_amt = float(input(
+#         #    "Since the dataset is new, how many time units (in minutes) do you expect to complete this task? "))
 
-    return user_input_data, time_amt
+#     return user_input_data  # , time_amt
 
 
 def save_file(filename, user_input_data):

task.py
@@ -1,35 +1,70 @@
 import pickle
 import config_files as cf
 from methods import *
+import os
 
 #########################################################
 # THIS IS THE CLASS FOR GETTING THE INFORMATION
-# THIS IS CURRENTLY NOT BEING USED IN THE MAIN PORTION AND NEEDS TO BE INCLUDED
 
 
 class Task_Information:
-    def __init__(self, name, category, time_amt, num_lines, num_segments, num_iterations, display_lines):
+    def __init__(self, name, category, num_lines, num_segments, num_iterations, display_lines):
         self.name = name
         self.category = category
-        self.time_amt = time_amt
         self.num_lines = num_lines
+        # The following variable is for storing how many segments have been completed
+        # (Note: It will only be used on the front end and not the graphical plot.
+        #        The graphical plot will only use num_segments)
+        self.num_completed_segments = 0
         self.num_segments = num_segments
         self.num_iterations = num_iterations
         self.display_lines = display_lines
 
-        self.filename = f"csv_files/{cf.FILE_NAME}"
-        self.user_input_data = np.array([])
+        self.filename = f"csv_files/{category}.csv"
+        # self.user_input_data = np.array([])
+        self.user_input_data = self.file_exists()
+
+    def file_exists(self):
+        """ Check if the file exists """
+
+        if os.path.exists(self.filename):
+            # Load previous data
+            self.user_input_data = np.loadtxt(self.filename, delimiter=',')
+
+        else:
+            # Create a new array
+            self.user_input_data = np.array([])
 
     def task_name(self):
         return self.name
 
-    def create_lines(self):
-        self.lines = conditions_met(self.time_amt, self.user_input_data,
-                                    self.num_lines, self.num_segments, self.num_iterations)
-# THIS IS A TEST FOR THE CLASS
-# test = Task_Information()
-# print(test.filename)
-# s
+    def category_type(self):
+        return self.category
+
+    def line_count(self):
+        return self.num_lines
+
+    def num_subtasks(self):
+        return self.num_segments
+
+    def decrement_subtasks(self):
+        if self.num_segments != 0:
+            self.num_segments -= 1
+
+    def iterations(self):
+        return self.num_iterations
+
+    def display_plot(self):
+        return self.display_lines
+
+    def file(self):
+        return self.filename
+
+    def data(self):
+        return self.user_input_data
+
+    def set_data(self, new_data):
+        self.user_input_data = new_data
 
 
 class Schedule:

time_manager.py
@@ -6,10 +6,11 @@ import config_files as cf
 
 # TEST
 from task import *
+from computation import *
 schedule = Schedule()
-task_info1 = Task_Information("test", "test", 1, 1, 1, 1, True)
+task_info1 = Task_Information("Read Book", "reading", 1, 1, 1, 1, True)
 task_info1.create_lines()
-task_info2 = Task_Information("hello", "test", 1, 1, 1, 1, True)
+task_info2 = Task_Information("Write Paper", "writing", 1, 1, 1, 1, True)
 task_info2.create_lines()
 schedule.add_task(task_info1)
 schedule.add_task(task_info2)
@@ -18,6 +19,9 @@ schedule.print_names()
 schedule.from_file()
 schedule.remove_task(task_info1)
 
+
+test = Computation(task_info1)
+print(test.file_name)
 # TEST
 
 
@@ -33,7 +37,8 @@ def running_code():
     np.set_printoptions(precision=14, suppress=True)
 
     # FILE_EXISTS FUNCTION WAS DEFINED HERE
-    user_input_data, time_amt = file_exists(filename)
+    # user_input_data, time_amt = file_exists(filename)
+    # user_input_data = file_exists(filename)
 
     # USER_QUESTION FUNCTION WAS DEFINED HERE
     num_lines, num_segments, num_iterations = user_questions()
@@ -43,8 +48,8 @@ def running_code():
     start_time = datetime.now()
 
     # CONDITIONS_MET FUNCTION WAS DEFINED HERE
-    lines = conditions_met(time_amt, user_input_data,
-                           num_lines, num_segments, num_iterations)
+    # lines = conditions_met(time_amt, user_input_data,
+    #                        num_lines, num_segments, num_iterations)
 
     # CREATE A FUNCTION HERE UNTIL THE END OF THIS FILE CALLED "start_program"
 